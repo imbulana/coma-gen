b61bf76a63689323eb23d12feb075970108f140d
imbulana <damithckumara@gmail.com>
2025-08-05 02:43:59 -0400

    update config

--- config.py
+++ config.py
@@ -1,34 +1,39 @@
+import torch
 from pathlib import Path
 from datetime import datetime
-import torch
 
-MAESTRO_DATA_PATH = Path("data/maestro-v3.0.0").resolve()
-MAESTRO_CSV = MAESTRO_DATA_PATH / "maestro-v3.0.0.csv"
-
-# SPLIT_DATA = True if not os.path.exists(MAESTRO_DATA_PATH / "splits") else False
-SPLIT_DATA = False
-LABEL_COMPOSER = False # for classification task
-AUGMENT_DATA = False
-
-GEN_PATH = Path("generated").resolve()
-GEN_PATH.mkdir(exist_ok=True)
-
-LOG_DIR = Path("logs").resolve() / datetime.now().strftime("%Y%m%d_%H%M%S")
-LOG_DIR.mkdir(exist_ok=True)
-
-NUM_BATCHES = int(1e5)
-BATCH_SIZE = 8
-GRADIENT_ACCUMULATE_EVERY = 4
-LEARNING_RATE = 2e-4
-VALIDATE_EVERY  = 150 # ~1 epoch for maestro
-GENERATE_EVERY  = 25
-GENERATE_LENGTH = 1024
-MAX_SEQ_LEN = 1024
+SEED = 42
 DEVICE = (
     'cuda' if torch.cuda.is_available() else
     'mps' if torch.backends.mps.is_available() else
     'cpu'
 )
+
+# data paths
+
+MAESTRO_DATA_PATH = Path("data/maestro-v3.0.0").resolve()
+MAESTRO_CSV = MAESTRO_DATA_PATH / "maestro-v3.0.0.csv"
+# MAESTRO_CSV = MAESTRO_DATA_PATH / "maestro-v3.0.0_standardized.csv"
+
+# data splitting
+
+SPLIT_DATA = False
+MAX_SEQ_LEN = 1024
+SHUFFLE = True
+SORT_BY = 'compositions' # must be in ['compositions', 'duration']
+TEST_SIZE = 0.2
+TOP_K_COMPOSERS = 10 # select top K composers by SORT_BY type to train/test on
+TO_SKIP = [] # composers to skip
+AUGMENT_DATA = False
+
+# tokenizer
+
+USE_PRETRAINED_TOKENIZER = False
+TOKENIZER_LOAD_PATH = Path("tokenizer.json").resolve() # pretrained tokenizer path
+
+TRAIN_TOKENIZER = False # whether to train the tokenizer to a target vocab size with byte pair encoding
+VOCAB_SIZE = 5000 # target vocab size for tokenizer training
+TOKENIZER_SAVE_PATH = Path("tokenizer.json").resolve()
 
 BEAT_RES = {(0, 1): 12, (1, 2): 4, (2, 4): 2, (4, 8): 1}
 TOKENIZER_PARAMS = {
@@ -45,7 +50,48 @@ TOKENIZER_PARAMS = {
     "tempo_range": (50, 200),  # (min_tempo, max_tempo)
 }
 
-# generate params
+# model
 
-TEMPERATURE = 0.7
+DIM = 512
+DEPTH = 4
+CAUSAL = True
+USE_DYNAMIC_POS_BIAS = True
+ATTN_WINDOW_SIZE = 256
+
+# ATTN_WINDOWS_SIZES = [32, 64]
+
+# CONV_EXPANSION_FACTOR = 2
+# CONV_KERNEL_SIZE = 4
+
+# ATTN_DROPOUT = 0.1
+# FF_DROPOUT = 0.1
+# CONV_DROPOUT = 0.1
+
+# training
+
+NUM_BATCHES = int(1e5)
+BATCH_SIZE = 8
+
+GRADIENT_ACCUMULATE_EVERY = 4
+VALIDATE_EVERY  = 150 # ~1 epoch for maestro
+
+GENERATE_EVERY  = 25
+GENERATE_LENGTH = 1024
+
+LEARNING_RATE = 2e-4
+# LR_SCHEDULER = None # must be in ["CosineAnnealingLR", "MultiStepLR", None]
+# MILESTONES = [7, 14] # for MultiStepLR
+# WEIGHT_DECAY = 2e-4 # 4e-4
+
+MAX_GRAD_NORM = None # for gradient clipping, set to None to disable
+
+# tensorboard logs
+
+datetime_str = datetime.now().strftime("%Y%m%d_%H%M%S")
+LOG_DIR = Path("logs") / f"k={TOP_K_COMPOSERS}_{datetime_str}"
+GEN_DIR = LOG_DIR / "gen"
+
+# generator params
+
+TEMPERATURE = 0.1
 FILTER_THRES = 0.9
---
+++ utils.py
@@ -0,0 +1,43 @@
+import json
+
+from config import *
+
+# data utils
+
+def save_config(writer):
+    config_dict = {
+        "SEED": SEED,
+        "DEVICE": str(DEVICE),
+
+        "SPLIT_DATA": SPLIT_DATA,
+        "SHUFFLE": SHUFFLE,
+        "TEST_SIZE": TEST_SIZE,
+        "TOP_K_COMPOSERS": TOP_K_COMPOSERS,
+        "TO_SKIP": TO_SKIP,
+        "AUGMENT_DATA": AUGMENT_DATA,
+
+        "TRAIN_TOKENIZER": TRAIN_TOKENIZER,
+        "VOCAB_SIZE": VOCAB_SIZE,
+
+        "BEAT_RES": str(BEAT_RES),
+        "TOKENIZER_PARAMS": {k: str(v) for k, v in TOKENIZER_PARAMS.items()},
+
+        "BATCH_SIZE": BATCH_SIZE,
+        "LEARNING_RATE": LEARNING_RATE,
+        "MAX_SEQ_LEN": MAX_SEQ_LEN,
+        "MAX_GRAD_NORM": MAX_GRAD_NORM,
+
+        "MODEL_PARAMS": {
+            "DIM": DIM,
+            "DEPTH": DEPTH,
+            "CAUSAL": CAUSAL,
+            "USE_DYNAMIC_POS_BIAS": USE_DYNAMIC_POS_BIAS,
+            "ATTN_WINDOW_SIZE": ATTN_WINDOW_SIZE,
+        },
+
+        "GENERATE_PARAMS": {
+            "TEMPERATURE": TEMPERATURE,
+            "FILTER_THRES": FILTER_THRES,
+        },
+    }
+    writer.add_text("config", json.dumps(config_dict, indent=2))
