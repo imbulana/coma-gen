e1a75bea927e46b2bc2eb5e788ace178abfde72d
imbulana <damithckumara@gmail.com>
2025-08-05 03:22:36 -0400

    update data splitting, add seed

--- train.py
+++ train.py
@@ -1,32 +1,42 @@
 import os
 import random
-import tqdm
-import pandas as pd
 import shutil
+import pandas as pd
+from tqdm import tqdm
 
 import torch
 from torch.optim import Adam
-from torch.utils.data import DataLoader, Dataset
+from torch.utils.data import DataLoader
 from torch.nn import functional as F
 from einops import rearrange
-
-from torch.utils.tensorboard import SummaryWriter
-from datetime import datetime
-
-from src import LocalTransformer
-
-from pathlib import Path
+from sklearn.model_selection import train_test_split
 
 from miditok import REMI, TokenizerConfig
 from miditok.pytorch_data import DatasetMIDI, DataCollator
 from miditok.utils import split_files_for_training
 from miditok.data_augmentation import augment_dataset
 
+from torch.utils.tensorboard import SummaryWriter
+
+from src import LocalTransformer
+
 # load config
 
 from config import *
+from utils import save_config
 
 writer = SummaryWriter(log_dir=LOG_DIR)
+save_config(writer)
+
+# create log dir
+
+os.makedirs(LOG_DIR, exist_ok=True)
+os.makedirs(GEN_DIR, exist_ok=True)
+
+# set seed
+
+random.seed(SEED)
+torch.manual_seed(SEED)
 
 # helpers
 
@@ -51,43 +61,126 @@ def top_k(logits, thres = 0.9):
 def extract_pitches_from_tokens(tokens, tokenizer):
     pass
 
-# load tokenizer and maestro data
+# load/train tokenizer and maestro data
 
-config = TokenizerConfig(**TOKENIZER_PARAMS)
-tokenizer = REMI(config)
+if USE_PRETRAINED_TOKENIZER and TOKENIZER_LOAD_PATH.exists():
+    print(f"\nloading pretrained tokenizer from {TOKENIZER_LOAD_PATH}\n")
+    tokenizer = REMI(params=TOKENIZER_LOAD_PATH)
+else:
+    config = TokenizerConfig(**TOKENIZER_PARAMS)
+    tokenizer = REMI(config)
+    print(f"\nusing base tokenizer with vocab size: {len(tokenizer)}\n")
 
 if SPLIT_DATA:
+
     split_parent_path = MAESTRO_DATA_PATH / "splits"
     df = pd.read_csv(MAESTRO_CSV)
+
+    if SORT_BY == 'compositions':
+        # select top k composers by number of compositions
+        composer_n_compositions = df.groupby('canonical_composer')['canonical_title'].nunique()
+        top_k_composers = composer_n_compositions.sort_values(ascending=False).head(TOP_K_COMPOSERS).index
+
+    elif SORT_BY == 'duration':
+        # select top k composers by duration
+        composer_duration = df.groupby('canonical_composer')['duration'].sum()
+        top_k_composers = composer_duration.sort_values(ascending=False).head(TOP_K_COMPOSERS).index
+    else:
+        raise ValueError(f"Invalid sort_by: {SORT_BY}. Must be 'compositions' or 'duration'.")
+
+    if TO_SKIP:
+        print(f"\nskipping composers: {TO_SKIP}\n")
+        top_k_composers = top_k_composers[~top_k_composers.isin(TO_SKIP)]
+
+    df = df[df['canonical_composer'].isin(top_k_composers)]
+    print(f"\nselected {len(top_k_composers)} composers: {top_k_composers.tolist()}\n")
+
+    # shuffle data (ensure that no composition is shared b/w train and valid)
+
+    if SHUFFLE:
+        print("\nshuffling data...\n")
+
+        for composer, df_composer in df.groupby('canonical_composer'):
+
+            titles = df_composer['canonical_title'].unique()
+            titles_train, titles_test = train_test_split(
+                titles, test_size=TEST_SIZE, random_state=SEED, shuffle=True
+            )
+
+            df.loc[df['canonical_title'].isin(titles_train), 'split'] = 'train'
+            df.loc[df['canonical_title'].isin(titles_test), 'split'] = 'validation'
+
+            assert set(titles_train) & set(titles_test) == set(), 'overlapping titles b/w train and test'
+
+        # # save new split summary
+
+        # split_summary = plot_data_split(df, LOG_DIR)
+        # print("\n# compositions per composer:\n")
+        # print(split_summary)
+
+    # train tokenizer
+
+    if TRAIN_TOKENIZER:
+        print(f"\ntraining tokenizer to target vocab size: {VOCAB_SIZE}\n")
+
+        train_paths = df[df['split'] == 'train']['midi_filename'].apply(
+            lambda x: str(MAESTRO_DATA_PATH / x)
+        ).tolist()
+
+        # train the tokenizer with Byte Pair Encoding to build the vocabulary
+
+        tokenizer.train(
+            vocab_size=VOCAB_SIZE,
+            files_paths=train_paths,
+        )
+        tokenizer.save(TOKENIZER_SAVE_PATH)
+
+    # remove existing split
+
+    if split_parent_path.exists():
+        shutil.rmtree(split_parent_path)
+        print(f"\nremoved existing split: {split_parent_path}\n")
+
+    # create new split
+
+    print("\ncreating new split...\n")
+
+    failures = []
     for split, df_split in df.groupby("split"):
         split_path = split_parent_path / split
 
-        if split_path.exists():
-            shutil.rmtree(split_path)
-            print(f"removed existing split: {split_path}")
+        for composer, df_composer in df_split.groupby("canonical_composer"):
+            # if any(skip_composer in str(composer) for skip_composer in TO_SKIP):
+            #     continue
 
-        split_path.mkdir(parents=True, exist_ok=True)
+            composer_path = split_path / composer
+            midi_file_paths = df_composer["midi_filename"].apply(
+                lambda x: MAESTRO_DATA_PATH / x
+            ).tolist()
+            
+            for midi_file_path in midi_file_paths:
+                try:
+                    split_files_for_training(
+                        files_paths=[midi_file_path],
+                        tokenizer=tokenizer,
+                        save_dir=composer_path / midi_file_path.name,
+                        max_seq_len=MAX_SEQ_LEN,
+                        num_overlap_bars=2,
+                    )
+                except FileNotFoundError:
+                    failures.append(midi_file_path)
+                    continue
 
-        midi_file_paths = df_split["midi_filename"].apply(
-            lambda x: MAESTRO_DATA_PATH / x
-        ).tolist()
+    if failures:
+        print(f"\nfailed to split {len(failures)} files")
+        print(failures)
 
-        split_files_for_training(
-            files_paths=midi_file_paths,
-            tokenizer=tokenizer,
-            save_dir=split_path,
-            max_seq_len=MAX_SEQ_LEN,
-            num_overlap_bars=2,
-        )
+leaf = lambda split : f"splits/{split}/*/*/*.mid?"
+get_composer_label = lambda dummy1, dummy2, x: x.parent.parent.name # signature expected by DatasetMIDI
 
-
-midi_paths_train = list(MAESTRO_DATA_PATH.glob("splits/train/*/*.mid?"))
-midi_paths_valid = list(MAESTRO_DATA_PATH.glob("splits/validation/*/*.mid?"))
-midi_paths_test = list(MAESTRO_DATA_PATH.glob("splits/test/*/*.mid?"))
-
-print(f"\ntrain samples: {len(midi_paths_train)}")
-print(f"valid samples: {len(midi_paths_valid)}")
-print(f"test samples: {len(midi_paths_test)}")
+midi_paths_train = list(MAESTRO_DATA_PATH.glob(leaf("train")))
+midi_paths_valid = list(MAESTRO_DATA_PATH.glob(leaf("validation")))
+midi_paths_test = list(MAESTRO_DATA_PATH.glob(leaf("test")))
 
 if AUGMENT_DATA:
     augment_dataset(
@@ -97,8 +190,29 @@ if AUGMENT_DATA:
         duration_offsets=[-0.5, 0.5],
     )
 
-    midi_paths_train = list(MAESTRO_DATA_PATH.glob("splits/train/*/*.mid?"))
+    # augments = list(set(MAESTRO_DATA_PATH.glob(leaf("train"))) - set(midi_paths_train))
+
+    # df_augments = pd.DataFrame(augments, columns=["path"])
+    # df_augments['composer'] = df_augments['path'].apply(lambda x: x.parent.parent.name)
+
+    midi_paths_train = list(MAESTRO_DATA_PATH.glob(leaf("train")))
     print(f"\ntrain samples (augmentations added): {len(midi_paths_train)}")
+
+# NOTE: for testing only
+# split individual recordings instead of compositions 
+# note that there are multiple recordings of the same composition in the MAESTRO dataset
+_SHUFFLE_RECORDINGS = True
+if _SHUFFLE_RECORDINGS:
+    print("\nshuffling recordings...\n")
+    all_paths = midi_paths_train + midi_paths_valid + midi_paths_test
+    df_all = pd.DataFrame(all_paths, columns=["path"])
+    df_all['composer'] = df_all['path'].apply(lambda x: x.parent.parent.name)
+    X_train, X_test = train_test_split(
+        df_all['path'], stratify=df_all['composer'], random_state=SEED, shuffle=True
+    )
+
+    midi_paths_train = X_train.tolist()
+    midi_paths_valid = X_test.tolist()
 
 # define datasets and dataloaders
 
@@ -127,30 +241,35 @@ val_loader = cycle(DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False,
 
 model = LocalTransformer(
     num_tokens = len(tokenizer), # vocab size
-    dim = 144,
-    depth = 4,
-    causal = True,
-    local_attn_window_size = 64,
+    dim = DIM,
+    depth = DEPTH,
+    causal = CAUSAL,
+    local_attn_window_size = ATTN_WINDOW_SIZE,
     # local_attn_window_sizes = ATTN_WINDOW_SIZES,
     max_seq_len = MAX_SEQ_LEN,
-    use_dynamic_pos_bias = True,
+    use_dynamic_pos_bias = USE_DYNAMIC_POS_BIAS,
     ignore_index = tokenizer["PAD_None"]
 ).to(DEVICE)
 
 print(f"\nmodel size: {sum(p.numel() for p in model.parameters()):,}")
 
-# optimizer
+# optimizer and loss
 
 optim = Adam(model.parameters(), lr=LEARNING_RATE) # TODO: try AdamW
+criterion = lambda logits, labels: F.cross_entropy(
+    rearrange(logits, 'b n c -> b c n'),
+    labels,
+    ignore_index = tokenizer["PAD_None"]
+)
 
 # training
 
 constant_seed = random.choice(val_dataset)['input_ids'].to(DEVICE)
 constant_seed_midi = decode_tokens(constant_seed.tolist(), tokenizer)
-constant_seed_midi.dump_midi(GEN_PATH / f"0_const_seed.mid")
+constant_seed_midi.dump_midi(GEN_DIR / f"0_const_seed.mid")
 
 best_val_loss = float('inf')
-for i in tqdm.tqdm(range(NUM_BATCHES), mininterval=10., desc='training'):
+for i in tqdm(range(NUM_BATCHES), mininterval=10., desc='training'):
     model.train()
     
     train_loss = 0.
@@ -164,13 +283,9 @@ for i in tqdm.tqdm(range(NUM_BATCHES), mininterval=10., desc='training'):
 
         logits = model(inp, mask=mask)
 
-        loss = F.cross_entropy(
-            rearrange(logits, 'b n c -> b c n'),
-            labels,
-            ignore_index = tokenizer["PAD_None"]
-        )
-
+        loss = criterion(logits, labels)
         loss.backward()
+
         train_loss += loss.item()
 
         # filtered_logits = top_k(logits[:, -1], thres = FILTER_THRES)
@@ -207,11 +322,7 @@ for i in tqdm.tqdm(range(NUM_BATCHES), mininterval=10., desc='training'):
             mask = mask[:, :-1]
 
             logits = model(inp, mask=mask)
-            val_loss = F.cross_entropy(
-                rearrange(logits, 'b n c -> b c n'),
-                labels,
-                ignore_index = tokenizer["PAD_None"]
-            )
+            val_loss = criterion(logits, labels)
             
             # val_melody_consistency = calculate_batch_melody_consistency(inp, model, tokenizer)
             # print(f'validation melody consistency: {val_melody_consistency:.4f}')
@@ -244,11 +355,11 @@ for i in tqdm.tqdm(range(NUM_BATCHES), mininterval=10., desc='training'):
 
             gen_const = model.generate(constant_seed[None, ...], GENERATE_LENGTH, temperature=TEMPERATURE)
             gen_const_midi = decode_tokens(gen_const[0].tolist(), tokenizer)
-            gen_const_midi.dump_midi(GEN_PATH / f"{i}_const_generated.mid")
+            gen_const_midi.dump_midi(GEN_DIR / f"{i}_const_generated.mid")
 
             combined_const = torch.cat((constant_seed, gen_const[0]))
             combined_const_midi = decode_tokens(combined_const.tolist(), tokenizer)
-            combined_const_midi.dump_midi(GEN_PATH / f"{i}_const_combined.mid")
+            combined_const_midi.dump_midi(GEN_DIR / f"{i}_const_combined.mid")
 
             # melody_consistency_const = calculate_melody_consistency_score(
             #     constant_seed.tolist(), gen_const[0].tolist(), tokenizer
@@ -264,9 +375,9 @@ for i in tqdm.tqdm(range(NUM_BATCHES), mininterval=10., desc='training'):
             generated_midi = decode_tokens(generated[0].tolist(), tokenizer)
             combined_midi = decode_tokens(combined.tolist(), tokenizer)
 
-            seed_midi.dump_midi(GEN_PATH / f"{i}_seed.mid")
-            generated_midi.dump_midi(GEN_PATH / f"{i}_generated.mid")
-            combined_midi.dump_midi(GEN_PATH / f"{i}_combined.mid")
+            seed_midi.dump_midi(GEN_DIR / f"{i}_seed.mid")
+            generated_midi.dump_midi(GEN_DIR / f"{i}_generated.mid")
+            combined_midi.dump_midi(GEN_DIR / f"{i}_combined.mid")
 
             print("\ngeneration complete\n")
 
